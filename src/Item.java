18d0913af7b023a58997a3a1407f07b7f35a63e8
SlabOMeat23 <75637438+SlabOMeat23@users.noreply.github.com>
2020-12-15 16:13:39 -0500

    removes properties

--- src/Item.java
+++ src/Item.java
@@ -1,17 +1,16 @@
 class Item{
     //Name of Item
     private String name;
-    //properties of item
-    private String prop; // may be changed later
+    
     //description of item
     private String des; // may also change later
 
   
     //There may also be a method to get photos of item
 
-    public Item(String n, String p, String d){
+    public Item(String n, String d){
         name = n;
-        prop = p;
+        
         des = d;
 
     }
@@ -20,9 +19,7 @@ class Item{
     public String getName(){
         return name;
     }
-    public String getProps(){
-        return prop;
-    }
+    
     public String getDes(){
         return des;
     }
