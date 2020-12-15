a261154dc285d88a8f10fe540529b058e0b2faa0
GreatJavaGuru <jlangsdale2021@gmail.com>
2020-12-15 14:31:15 -0500

    Window to display store

--- ICTOAGN/src/display/Window.java
+++ ICTOAGN/src/display/Window.java
@@ -1,18 +1,14 @@
 package display;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
+
+import pages.Product;
+import pages.index;
 
 
 public class Window {
@@ -33,31 +29,18 @@ public class Window {
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		
-		Container mainContainer = frame.getContentPane();
-		mainContainer.setLayout(new BorderLayout(8, 6));
+		frame.setContentPane(new index(this));
 		frame.getRootPane().setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.GREEN));
 		
-		JPanel topPanel = new JPanel();
-		topPanel.setBorder(new LineBorder(Color.BLACK, 3));
-		topPanel.add(new JLabel("ICTOAGN"));
-		mainContainer.add(topPanel, BorderLayout.NORTH);
-		
-		JLabel furniture = new JLabel("Furniture:");
-		
-		JPanel center = new JPanel();
-		JPanel department = new JPanel();
-		center.setBorder(new LineBorder(Color.WHITE, 3));
-		center.add(furniture);
-		center.setLayout(new FlowLayout(5));
-		department.setLayout(new FlowLayout(5));
-		department.add(new JLabel(new ImageIcon("res/chair.jpg")));
-		department.add(new JLabel(new ImageIcon("res/lamp.jpg")));
-		department.add(new JLabel(new ImageIcon("res/chair.jpg")));
-		department.add(new JLabel(new ImageIcon("res/lamp.jpg")));
-		center.add(department);
-		mainContainer.add(center, BorderLayout.CENTER);
-		
 		frame.setVisible(true);
+	}
+	
+	public void setHome() {
+		frame.setContentPane(new index(this));
+	}
+	
+	public void setProduct() {
+		frame.setContentPane(new Product(1, this));
 	}
 	
 }
